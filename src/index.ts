f2151861731ee10c760c668b6bb4e0f677696aec
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2022-10-24 17:35:59 +0200

    refactor: update code

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,24 @@
-type Unit =
-  | 'B'
-  | 'Byte'
-  | 'Bytes'
-  | 'KB'
-  | 'Kilobyte'
-  | 'Kilobytes'
-  | 'MB'
-  | 'Megabyte'
-  | 'Megabytes'
-  | 'GB'
-  | 'Gigabyte'
-  | 'Gigabytes'
-  | 'TB'
-  | 'Terabyte'
-  | 'Terabytes'
-  | 'PB'
-  | 'Petabyte'
-  | 'Petabytes'
-  | 'EB'
-  | 'Exabyte'
-  | 'Exabytes'
-  | 'ZB'
-  | 'Zettabyte'
-  | 'Zettabytes'
-  | 'YB'
-  | 'Yottabyte'
-  | 'Yottabytes'
-
-const kb = 1000
-, mb = kb * 1000
-, gb = mb * 1000
-, tb = gb * 1000
-, pb = tb * 1000
-, eb = pb * 1000
-, zb = eb * 1000
-, yb = zb * 1000
-
-const units: [string, string, number][] = [
-  ['B', 'Byte', 1],
-  ['KB', 'Kilobyte', kb],
-  ['MB', 'Megabyte', mb],
-  ['GB', 'Gigabyte', gb],
-  ['TB', 'Terabyte', tb],
-  ['PB', 'Petabyte', pb],
-  ['EB', 'Exabyte', eb],
-  ['ZB', 'Zettabyte', zb],
-  ['YB', 'Yottabyte', yb]
-]
+import { Unit } from './unit'
+import { EB, EiB, GB, GiB, KB, KiB, MB, MiB, PB, PiB, TB, TiB, units, YB, YiB, ZB, ZiB } from './utility'
 
 /**
- * Parse or format the given value.
- * 
- * @param value - The string or number to convert.
- * @param config - The configuration for the conversion.
+ * parse or format the given value.
  */
-function bytes(value: number, config?: { long?: boolean, format?: 'string' }): `${number} ${Unit}`
-function bytes(value: number, config?: { long?: boolean, format?: 'array' }): [number, Unit]
+function bytes(value: number, options?: { long?: boolean, format?: 'string', prefix?: 'metric' | 'binary' }): `${number} ${Unit}`
+function bytes(value: number, options?: { long?: boolean, format?: 'array', prefix?: 'metric' | 'binary' }): [number, Unit]
 function bytes(value: `${number} ${Unit}`): number
 function bytes(
   value: `${number} ${Unit}` | number,
-  config?: {
+  options?: {
     long?: boolean
     format?: 'string' | 'array'
+    prefix?: 'metric' | 'binary'
   }
 ): `${number} ${Unit}` | [number, Unit] | number {
+  const long = options?.long ?? false
+  , array = options?.format === 'array'
+  , binary = options?.prefix === 'binary'
+
   if (typeof value === 'number') {
     const stringifiedSize: string = value.toString()
     , length = stringifiedSize.length
@@ -83,46 +38,46 @@ function bytes(
       shortName: Unit
     ): `${number} ${Unit}` | [number, Unit] => {
       const roundedValue = round(value / divisor, 2)
-      , name = (config?.long ? `${longName}${roundedValue > 1 ? 's' : ''}` : shortName) as Unit
+      , name = (long ? `${longName}${roundedValue > 1 ? 's' : ''}` : shortName) as Unit
 
-      return config?.format === 'array' ? [roundedValue, name] : `${roundedValue} ${name}`
+      return array ? [roundedValue, name] : `${roundedValue} ${name}`
     }
 
     return (length >= 4 && length < 7) ? (
-      bytes(kb, 'Kilobyte', 'KB')
+      binary ? bytes(KiB, 'Kibibyte', 'KiB') : bytes(KB, 'Kilobyte', 'KB')
     ) : (length >= 7 && length < 10) ? (
-      bytes(mb, 'Megabyte', 'MB')
+      binary ? bytes(MiB, 'Mebibyte', 'MiB') : bytes(MB, 'Megabyte', 'MB')
     ) : (length >= 10 && length < 13) ? (
-      bytes(gb, 'Gigabyte', 'GB')
+      binary ? bytes(GiB, 'Gibibyte', 'GiB') : bytes(GB, 'Gigabyte', 'GB')
     ) : (length >= 13 && length < 16) ? (
-      bytes(tb, 'Terabyte', 'TB')
+      binary ? bytes(TiB, 'Tebibyte', 'TiB') : bytes(TB, 'Terabyte', 'TB')
     ) : (length >= 16 && length < 19) ? (
-      bytes(pb, 'Petabyte', 'PB')
+      binary ? bytes(PiB, 'Pebibyte', 'PiB') : bytes(PB, 'Petabyte', 'PB')
     ) : (length >= 19 && length < 22) ? (
-      bytes(eb, 'Exabyte', 'EB')
+      binary ? bytes(EiB, 'Exbibyte', 'EiB') : bytes(EB, 'Exabyte', 'EB')
     ) : (length >= 22 && length < 25) ? (
-      bytes(zb, 'Zettabyte', 'ZB')
+      binary ? bytes(ZiB, 'Zebibyte', 'ZiB') : bytes(ZB, 'Zettabyte', 'ZB')
     ) : (length >= 25) ? (
-      bytes(yb, 'Yottabyte', 'YB')
+      binary ? bytes(YiB, 'Yobibyte', 'YiB') : bytes(YB, 'Yottabyte', 'YB')
     ) : (
       bytes(1, 'Byte', 'B')
     )
   } else {
-    const array = value.split(' ')
+    const p = value.split(' ')
 
-    if (array.length !== 2)
+    if (p.length !== 2)
       return -1
 
-    if (array[1].endsWith('s'))
-      array[1] = array[1].slice(0, -1)
+    if (p[1].endsWith('s'))
+      p[1] = p[1].slice(0, -1)
 
     const length = units.length
     let i = 0
     , bytes = -1
 
     while (i < length) {
-      if (array[1] === units[i][0] || array[1] === units[i][1])
-        bytes = parseInt(array[0]) * units[i][2]
+      if (p[1] === units[i][0] || p[1] === units[i][1])
+        bytes = parseInt(p[0]) * units[i][2]
 
       i++
     }
